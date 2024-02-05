7d3ab0884308dba0c3e794c132e51038cfac166f
Andreas Bergström <andreas@textalkgroup.com>
2024-02-05 09:18:19 +0100

    fixes

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let tasks = ["Tvätta", "Laga bilen", "Skjutsa barn", "Spela sällskapsspel"];
+const taskDone = document.getElementById("task-done");
+
+function createTask(callbackFn) {}
+function deleteFinishedTasks(callbackFn) {}
+function editTask(callbackFn) {}
+function loadTasks() {}
